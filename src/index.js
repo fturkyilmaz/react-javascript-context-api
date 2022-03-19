822a13728bfc592d29302cf2bac5c9fc598e9dc9
Furkan Türkyılmaz <trkyilmazfurkan@gmail.com>
2022-03-19 10:02:31 +0300

    Provider commit added.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ContextProvider } from "./context";
 
+// Projemizi ContextProvider ile bağlayalım.
+
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
@@ -14,7 +16,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
