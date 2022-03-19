56b04afc9406115cbfb2bc4d99e5cb5fc01976b2
Furkan Türkyılmaz <trkyilmazfurkan@gmail.com>
2022-03-19 09:59:24 +0300

    Init Project Context Api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useContext, MainContext } from "../src/context";
 
 function App() {
+  const [state, dispatch] = useContext(MainContext);
+
+  const { theme, message, linkedin } = state;
+
+  console.log("Context", state);
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>{message}</h3>
+
+        <div className="buttonContainer">
+          <button
+            onClick={() => dispatch({ type: "TOGGLE_THEME" })}
+            className="button">
+            Toggle Theme
+          </button>
+
+          <button
+            onClick={() =>
+              dispatch({
+                type: "TOGGLE_MESSAGE",
+                payload: "Hi everyone! I'm Furkan TÜRKYILMAZ",
+              })
+            }
+            className="button">
+            Change Message
+          </button>
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={linkedin}
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          rel="noopener noreferrer">
+          LinkedIn
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ContextProvider } from "./context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
